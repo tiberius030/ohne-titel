d766e2183b83d9db7f34d24bf3f3dd70c0dab585
tiberius030 <153438924+tiberius030@users.noreply.github.com>
2023-12-12 17:32:05 +0100

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,14 @@
 input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
-    for (let index = 0; index < 6; index++) {
-        basic.pause(10000)
-        music.playTone(262, music.beat(BeatFraction.Whole))
-        basic.setLedColor(0xff0000)
-        basic.clearScreen()
+    if (input.buttonIsPressed(Button.A)) {
+        for (let index = 0; index < 6; index++) {
+            basic.pause(10000)
+            basic.setLedColor(0xff0000)
+            music.playTone(262, music.beat(BeatFraction.Whole))
+            basic.clearScreen()
+        }
+    } else {
+        basic.showIcon(IconNames.ArrowNorth)
+        basic.showIcon(IconNames.ArrowEast)
     }
 })
 input.onButtonEvent(Button.AB, input.buttonEventClick(), function () {
@@ -22,6 +27,7 @@ input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
         basic.clearScreen()
     }
 })
+basic.showIcon(IconNames.ArrowEast)
 music.play(music.builtinPlayableSoundEffect(soundExpression.giggle), music.PlaybackMode.UntilDone)
 music.play(music.createSoundExpression(WaveShape.Square, 1600, 1, 255, 0, 300, SoundExpressionEffect.None, InterpolationCurve.Curve), music.PlaybackMode.UntilDone)
 basic.showIcon(IconNames.Yes)
